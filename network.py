ddf3fbb94581102ac553664b01234d18fabdd313
do.maggihoo <Maggi3282>
2019-11-01 14:52:19 +0800

    conv

--- network.py
+++ network.py
@@ -327,10 +327,10 @@ class Encoder(nn.Module):
 
     def first_block(self):
         layers = []
-        layers = conv(layers, 3, 128, 5, 1, 0, self.flag_leaky, self.flag_bn, self.flag_wn,
+        layers = conv(layers, 3, 128, 5, 2, 0, self.flag_leaky, self.flag_bn, self.flag_wn,
                         self.flag_pixelwise)
-        layers = conv(layers, 128, 256, 5, 1, 0, self.flag_leaky, self.flag_bn, self.flag_wn, self.flag_pixelwise)
-        layers = conv(layers, 256, 512, 3, 1, 0, self.flag_leaky, self.flag_bn, self.flag_wn, self.flag_pixelwise)
+        layers = conv(layers, 128, 256, 5, 2, 0, self.flag_leaky, self.flag_bn, self.flag_wn, self.flag_pixelwise)
+        layers = conv(layers, 256, 512, 3, 2, 0, self.flag_leaky, self.flag_bn, self.flag_wn, self.flag_pixelwise)
         #layers = linear(layers, 512, 1024, sig=self.flag_sigmoid, wn=self.flag_wn)
         return nn.Sequential(*layers), 1024
 
